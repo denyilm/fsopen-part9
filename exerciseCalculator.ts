cce573a781f45bbc993e0f14c24534313cdf9145
deniz <yilmaz.deniz.itu@gmail.com>
2024-11-07 12:53:02 +0300

    solution: 9.3

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -8,7 +8,33 @@ interface Result {
     average: number
 }
 
-const calculateExercises = (dailyExercises: number[], target: number): Result => {
+interface ExerciseInput {
+    target: number,
+    dailyExercises: number[]
+}
+
+const parseArgumentsForArrays = (args: string[]): ExerciseInput => {
+    if (args.length < 3) throw new Error('Not enough arguments');
+    let dailyExercises_ = args.slice(3).map((b:string)=>{
+        return Number(b)
+    })
+    let allNumber = true
+    dailyExercises_.forEach((a:number)=>{
+        if(!isNaN(Number(a))){
+            allNumber = false
+        }
+    })
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+      return {
+        target: Number(args[2]),
+        dailyExercises: dailyExercises_
+      }
+    } else {
+      throw new Error('Provided values were not numbers!');
+    }
+  }
+
+const calculateExercises = (target: number, dailyExercises: number[]): Result => {
     let average = dailyExercises.reduce((a: number,b: number) => a+b)/dailyExercises.length
     let rating: number = 1
     let description: string = "bad"
@@ -33,4 +59,13 @@ const calculateExercises = (dailyExercises: number[], target: number): Result =>
     }
 }
 
-console.log(calculateExercises([0, 0, 0, 1, 0, 3, 1],2))
+try {
+    const { target, dailyExercises } = parseArgumentsForArrays(process.argv);
+    console.log(calculateExercises(target, dailyExercises))
+} catch (error: unknown) {
+    let errorMessage = 'Something bad happened.'
+    if (error instanceof Error) {
+        errorMessage += ' Error: ' + error.message;
+    }
+    console.log(errorMessage);
+}
